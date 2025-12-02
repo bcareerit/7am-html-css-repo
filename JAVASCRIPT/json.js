9f46541fffc563a80160dbc3a507ca93830a89ba
bcareerit <bcareerit@gmail.com>
2025-12-02 08:02:47 +0530

    json

---
+++ JAVASCRIPT/json.js
@@ -0,0 +1,111 @@
+// let obj1 = {
+//     "key1" : "Hello",
+//     "key2" : "JS"
+// };
+// let x1 = obj1.key1;
+// let y1 = obj1.key2;
+// console.log(x1,y1);
+
+// let x2 = obj1["key1"];
+// let y2 = obj1["key2"];
+// console.log(x2,y2);
+
+// let {key1,key2} = obj1;
+// console.log(key1,key2);
+
+// for(let key in obj1){
+//     console.log(obj1[key]);
+// }
+
+// let obj1 = {
+//     "key1" : "Hello",
+//     "key2" : "JS"
+// };
+// let keys = Object.keys(obj1);
+// console.log(keys);
+
+// let values = Object.values(obj1);
+// console.log(values);
+
+// let obj1 = {
+//     "key1" : "Hello",
+//     "key1" : "JS",
+//     "key2" : "React"
+// };
+// let {key1,key2} = obj1;
+// console.log(key1,key2);
+
+
+
+
+// let obj1 = {
+//     "key1" : "Hello",
+//     "key2" : "Hello"
+// };
+// let {key1,key2} = obj1;
+// console.log(key1,key2);
+
+
+// let obj1 = {
+//     "key1" : {
+//         "key2" : {
+//             "msg" : "Hello, JSON !!!"
+//         }
+//     }
+// };
+// let {key1} = obj1;
+// let {key2} = key1;
+// let {msg} = key2;
+// console.log(msg);
+
+
+// let obj1 = {
+//     obj1 : { //x
+//         obj1 : { //y
+//             msg : "Hello, JSON !!!"
+//         }
+//     }
+// };
+// let {obj1:x} = obj1;
+// let {obj1:y} = x;
+// let {msg} = y;
+// console.log(msg);
+
+// let obj1 = {
+//     key1 : "hello"
+// };
+// let obj2 = {
+//     key2 : "welcome"
+// };
+// let obj3 = {
+//     key3 : "JS"
+// };
+// let new_obj = {...obj1,...obj2,...obj3};
+// console.log(new_obj);
+
+// let new_obj1 = Object.assign(obj1,obj2,obj3);
+// console.log(new_obj1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
